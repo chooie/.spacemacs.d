bacc48fc8971104c517bc35783ecf4bc6229092a
Charlie Hebert <charlie.hebert92@gmail.com>
2017-05-18 22:12:44 -0600

    Fix indentation in init.el

--- init.el
+++ init.el
@@ -315,14 +315,14 @@ before packages are loaded. If you are unsure, you should try in setting them in
   )
 
 (defun my/use-eslint-from-node-modules ()
-    (let* ((root (locate-dominating-file
-                  (or (buffer-file-name) default-directory)
-                  "node_modules"))
-           (eslint (and root
-                        (expand-file-name "node_modules/eslint/bin/eslint.js"
-                                          root))))
-      (when (and eslint (file-executable-p eslint))
-        (setq-local flycheck-javascript-eslint-executable eslint))))
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
 
 (defun dotspacemacs/user-config ()
   "Configuration function for user code.
