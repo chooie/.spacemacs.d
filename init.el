75e2df857940ed1d67b8f30d8bc2979de7289276
chebert <chebert@spotxchange.com>
2017-06-28 14:23:18 -0600

    Add feature-mode for editing Cucumber feature files. Add vim commands for multicursors

--- init.el
+++ init.el
@@ -61,6 +61,7 @@ values."
    '(
      column-enforce-mode
      groovy-mode
+     feature-mode
      )
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
@@ -338,6 +339,7 @@ you should place your code here."
   (spacemacs/toggle-indent-guide-globally-on)
   (setq-default indent-tabs-mode nil)
   (setq-default tab-width 2)
+  (setq-default tab-always-indent t)
   ;; Minimal dired
   (add-hook 'dired-mode-hook
             (lambda ()
@@ -363,6 +365,29 @@ you should place your code here."
         '("node_modules"
           "babel-out"
           "coverage"))
+
+  ;; Prevent multicursors from prompting all the time when using evil
+  ;; commands
+  (setq mc/cmds-to-run-for-all
+        '(
+          electric-newline-and-maybe-indent
+          evil-backward-char
+          evil-delete-char
+          evil-escape-emacs-state
+          evil-escape-insert-state
+          evil-exit-emacs-state
+          evil-forward-char
+          evil-insert
+          evil-next-line
+          evil-normal-state
+          evil-previous-line
+          forward-sentence
+          kill-sentence
+          org-self-insert-command
+          sp-backward-delete-char
+          sp-delete-char
+          sp-remove-active-pair-overlay
+          ))
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
@@ -374,7 +399,7 @@ you should place your code here."
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (phpunit phpcbf php-auto-yasnippets drupal-mode php-mode ranger dockerfile-mode docker tablist docker-tramp insert-shebang fish-mode company-shell stickyfunc-enhance srefactor sql-indent groovy-mode flycheck-pos-tip pos-tip flycheck unfill mwim helm-company helm-c-yasnippet fuzzy company-web web-completion-data company-tern dash-functional tern company-statistics company auto-yasnippet ac-ispell auto-complete web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode helm-css-scss haml-mode emmet-mode smeargle orgit org-projectile org-present org-pomodoro alert log4e gntp org-download magit-gitflow htmlize helm-gitignore gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link evil-magit magit magit-popup git-commit with-editor reveal-in-osx-finder pbcopy osx-trash osx-dictionary launchctl mmm-mode markdown-toc markdown-mode gh-md web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor yasnippet multiple-cursors js2-mode js-doc coffee-mode ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed dash aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async)))
+    (feature-mode phpunit phpcbf php-auto-yasnippets drupal-mode php-mode ranger dockerfile-mode docker tablist docker-tramp insert-shebang fish-mode company-shell stickyfunc-enhance srefactor sql-indent groovy-mode flycheck-pos-tip pos-tip flycheck unfill mwim helm-company helm-c-yasnippet fuzzy company-web web-completion-data company-tern dash-functional tern company-statistics company auto-yasnippet ac-ispell auto-complete web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode helm-css-scss haml-mode emmet-mode smeargle orgit org-projectile org-present org-pomodoro alert log4e gntp org-download magit-gitflow htmlize helm-gitignore gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link evil-magit magit magit-popup git-commit with-editor reveal-in-osx-finder pbcopy osx-trash osx-dictionary launchctl mmm-mode markdown-toc markdown-mode gh-md web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor yasnippet multiple-cursors js2-mode js-doc coffee-mode ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed dash aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async)))
  '(sh-indentation 2 t)
  '(standard-indent 2))
 (custom-set-faces
