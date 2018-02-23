8f4f33baf60a695127fdf965d41277e02898f6f3
chooie <chebert@spotx.tv>
2018-02-23 14:21:44 -0700

    Workaround for multicursor refactorings

--- init.el
+++ init.el
@@ -414,28 +414,66 @@ you should place your code here."
                                  (aggressive-indent-mode)
                                  (put-clojure-indent 'fdef 'defun)))
 
-  ;; Prevent multicursors from prompting all the time when using evil
-  ;; commands
-  (setq mc/cmds-to-run-for-all
-        '(
-          electric-newline-and-maybe-indent
-          evil-backward-char
-          evil-delete-char
-          evil-escape-emacs-state
-          evil-escape-insert-state
-          evil-exit-emacs-state
-          evil-forward-char
-          evil-insert
-          evil-next-line
-          evil-normal-state
-          evil-previous-line
-          forward-sentence
-          kill-sentence
-          org-self-insert-command
-          sp-backward-delete-char
-          sp-delete-char
-          sp-remove-active-pair-overlay
-          ))
+  ;; =============================================================
+  ;; Multiple cursors evil compat (use emacs mode during mc)
+  ;; =============================================================
+  (defvar mc-evil-compat/evil-prev-state nil)
+  (defvar mc-evil-compat/mark-was-active nil)
+
+  (defun user-utils/evil-visual-or-normal-p ()
+    "True if evil mode is enabled, and we are in normal or visual mode."
+    (and (bound-and-true-p evil-mode)
+         (not (memq evil-state '(insert emacs)))))
+
+  (defun mc-evil-compat/switch-to-emacs-state ()
+    (when (user-utils/evil-visual-or-normal-p)
+
+      (setq mc-evil-compat/evil-prev-state evil-state)
+
+      (when (region-active-p)
+        (setq mc-evil-compat/mark-was-active t))
+
+      (let ((mark-before (mark))
+            (point-before (point)))
+
+        (evil-emacs-state 1)
+
+        (when (or mc-evil-compat/mark-was-active (region-active-p))
+          (goto-char point-before)
+          (set-mark mark-before)))))
+
+  (defun mc-evil-compat/back-to-previous-state ()
+    (when mc-evil-compat/evil-prev-state
+      (unwind-protect
+          (case mc-evil-compat/evil-prev-state
+            ((normal visual) (evil-force-normal-state))
+            (t (message "Don't know how to handle previous state: %S"
+                        mc-evil-compat/evil-prev-state)))
+        (setq mc-evil-compat/evil-prev-state nil)
+        (setq mc-evil-compat/mark-was-active nil))))
+
+  (add-hook 'multiple-cursors-mode-enabled-hook
+            'mc-evil-compat/switch-to-emacs-state)
+  (add-hook 'multiple-cursors-mode-disabled-hook
+            'mc-evil-compat/back-to-previous-state)
+
+  (defun mc-evil-compat/rectangular-switch-state ()
+    (if rectangular-region-mode
+        (mc-evil-compat/switch-to-emacs-state)
+      (setq mc-evil-compat/evil-prev-state nil)))
+
+  ;; When running edit-lines, point will return (position + 1) as a
+  ;; result of how evil deals with regions
+  (defadvice mc/edit-lines (before change-point-by-1 activate)
+    (when (user-utils/evil-visual-or-normal-p)
+      (if (> (point) (mark))
+          (goto-char (1- (point)))
+        (push-mark (1- (mark))))))
+
+  (add-hook 'rectangular-region-mode-hook
+            'mc-evil-compat/rectangular-switch-state)
+
+  (defvar mc--default-cmds-to-run-once nil)
 
   (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
   (setq TeX-view-program-list
